c76afb150030e1b26ab5253ae6eb2d6f6415a4af
matthewrpacker <matthewrpacker@gmail.com>
2016-10-03 21:32:50 -0600

    Add root page - User can see table of existing ideas - Add rating method to idea model - Add idea controller, action, view, route and spec (non-api)

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,14 @@ class Idea < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   validates :quality, presence: true
+
+  def rating
+    if self.quality == 0
+      "Swill"
+    elsif self.quality == 1
+      "Plausible"
+    else
+      "Genius"
+    end
+  end
 end
